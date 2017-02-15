8a4d2a375379420211987986067f53e544d945c9
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2017-02-15 16:04:23 +0800

    initial tunring operation

---
+++ CadCamProject/CadCamProject/Turning.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+namespace CadCamProject
+{
+    public  class Operation
+    {
+        //General Parameters
+        public string TypeImagineOperation { get; set; }
+        public Operations TypeOperation { get; set; }
+        public string Parameters { get; set; }
+        public int Index { get; set; }
+        public string dataContent { get; set; }
+
+        //Common Parameters
+        public string OperationName { get; set; }
+        public string version { get; set; }
+
+        public Operation()
+        {
+            TypeImagineOperation = "/Images/Profile.png";
+            TypeOperation = Operations.Profile;
+
+
+            dataContent = "vamos por buen camino kikin";
+        }
+
+        public Operation GetParameters(Main MainPage, int index)
+        {
+            Operation op = new Operation();
+
+            if (index <= MainPage.listViewOperations.Items.Count)
+            {
+                var listOperation = MainPage.listViewOperations.Items.GetItemAt(index) as List<Operation>;
+                op = listOperation.Last();
+            }
+            else
+            {
+                op.Index = MainPage.listViewOperations.Items.Count;
+            }
+            return op;
+        }
+
+        public List<Operation> SetParameters(Operation opParameters, Main MainPage)
+        {
+            List<Operation> listOperation = new List<Operation>();
+            listOperation.Add(opParameters);
+            if (opParameters.Index != MainPage.listViewOperations.Items.Count)
+            {
+                MainPage.listViewOperations.Items.RemoveAt(opParameters.Index);
+            }
+            return listOperation;
+        }
+
+    }
+}
